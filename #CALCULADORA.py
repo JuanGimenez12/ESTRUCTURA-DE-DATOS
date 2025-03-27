f54c87e202f560f42c3237413ea5be42d6bc1cbd
JuanGimenez12 <jdgimenez0@alumnos.unsada.edu.ar>
2025-03-27 15:43:37 -0300

    Calculadora INGENIERIA DE SOFTWARE Practico 1

--- #CALCULADORA.py
+++ #CALCULADORA.py
@@ -1,39 +1,98 @@
-#CALCULADORA
-def main():                                
-    historial = HistorialNavegacion()   #Creamos la Instancia de la clase, las paginas del Historial
+def suma():
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+    resultado = num1 + num2
+    print(f"El resultado de la suma es: {resultado}")
 
-    for i in range(3):                  #creamos un Bucle for que se va a repetir 3 veces, para ingresar 3 paginas al Historial
-        url = input(f"Ingrese la URL de la página {i+1}: ")  #Pedimos que ingresen por teclado la URL, para almacenarta como una instancia de la clase Pagina
-        pagina = Pagina(url)                         #Creamos una Instancia de la Pagina con la URL ingresada
-        historial.agregar_pagina(pagina)   #llamamos a la funcion agregar_pagina para agregar la pagina al historial
-        print(historial)                  #Imprimimos la pagina agregada como pagina actual
+def resta():
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+    resultado = num1 - num2
+    print(f"El resultado de la resta es: {resultado}")
 
-    while True:                          #Utilizamos un bucle while, para mostrar el Menu de opciones
-        print("\nOpciones:")     
-        print("1. Retroceder")
-        print("2. Avanzar")
-        print("3. Salir")
-        opcion = input("Ingrese su opción: ")  #espera la opcion a realizar 1,2 o 3
+def multiplicacion():
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+    resultado = num1 * num2
+    print(f"El resultado de la multiplicación es: {resultado}")
+
+def division():
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+    if num2 != 0:
+        resultado = num1 / num2
+        print(f"El resultado de la división es: {resultado}")
+    else:
+        print("Error: No se puede dividir por cero")
+
+def menu_principal():
+    while True:
+        print("\n       Calculadora Grupo 1 EDD     ")
+        print("-----------------------------------")
+        print("1. Suma")
+        print("2. Resta")
+        print("3. Multiplicación")
+        print("4. División")
+        print("5. Salir")
+        opcion = input("Ingrese su opción: ")
+
 
         if opcion == "1":
-            pagina = historial.retroceder()   #llama a la funcion retroceder 
-            if pagina:                        #Si hay una pagina la Imprime
-                print(historial)
+            suma()
+            print("1. Volver a Sumar")
+            print("2. Volver al menú principal")
+            sub_opcion = input("Ingrese su opción: ")
+            if sub_opcion == "1":
+                suma()
+            elif sub_opcion == "2":
+                return menu_principal()
             else:
-                print("No hay páginas atrás") #Si no imprime este mensaje, ya que no hay nada para mostrar
-                print(historial)              #Imprime la pagina Actual
+                print("Opción inválida. Regresando al menú principal.")
+
+
         elif opcion == "2":
-            pagina = historial.avanzar()      #llama a la funcion avanzar
-            if pagina:                        #la cual verifica que exista una paginaa la cual avanzar
-                print(historial)              #la imprime
+            resta()
+            print("1. Volver a Restar")
+            print("2. Volver al menú principal")
+            sub_opcion = input("Ingrese su opción: ")
+            if sub_opcion == "1":
+                resta()
+            elif sub_opcion == "2":
+                return menu_principal()
             else:
-                print("No hay páginas adelante") #Si no existe, imprime el mensaje
-                print(historial)              #Imprime la pagina Actual
-        elif opcion == "3":    #se detiene el ciclo al ingresar un 3 y cierra el menu
+                print("Opción inválida. Regresando al menú principal.")
+
+
+        elif opcion == "3":
+            multiplicacion()
+            print("1. Volver a Multiplicar")
+            print("2. Volver al menú principal")
+            sub_opcion = input("Ingrese su opción: ")
+            if sub_opcion == "1":
+                multiplicacion()
+            elif sub_opcion == "2":
+                return menu_principal()
+            else:
+                print("Opción inválida. Regresando al menú principal.")
+
+                
+        elif opcion == "4":
+            division()
+            print("1. Volver a Dividir")
+            print("2. Volver al menú principal")
+            sub_opcion = input("Ingrese su opción: ")
+            if sub_opcion == "1":
+                division()
+            elif sub_opcion == "2":
+                return menu_principal()
+            else:
+                print("Opción inválida. Regresando al menú principal.")
+
+
+        elif opcion == "5":
+            print("Hasta luego!")
             break
         else:
-            print("Opción inválida") #al ingresar una opcion que no existe imprime el mensaje
-            print(historial)              #Imprime la pagina Actual
+            print("Opción inválida. Por favor, inténtelo de nuevo.")
 
-if __name__ == "__main__":
-    main()
+menu_principal()
